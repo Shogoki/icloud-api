7cfe451cd3cfaa33d6be46f3b00ad7ae6f70bf60
Sven Kraus <mail@svenkraus.de>
2022-10-05 19:39:32 -0500

    enable cofrs and sort by date

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import icloud from 'npm:icloud-shared-album@1.1.0'
 import { Application, Router, helpers } from 'https://deno.land/x/oak/mod.ts';
 import { Derivative, ICloudResponse, ImageResponse } from './types.ts';
-import { isPosixPathSeparator } from 'https://deno.land/std@0.152.0/path/_util.ts';
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+//TODO: Read this from env
 const port = 8000;
 const app = new Application();
 
@@ -17,7 +18,10 @@ router.get("/album/:key", async (ctx) => {
     }
     // const data = await icloud.getImages('B0OGrq0zwH4gVC');
     
-    const resp: ImageResponse[] = data.photos.map(photo => {
+    const resp: ImageResponse[] = data.photos
+    // we want to sort by date created
+    .sort((a,b) => (a.dateCreated - b.dateCreated))
+    .map(photo => {
       const fullImage = Object.values(photo.derivatives).reduce(
        (prev, current) => {
          return prev.fileSize > current.fileSize ? prev : current
@@ -28,11 +32,18 @@ router.get("/album/:key", async (ctx) => {
          return prev.fileSize < current.fileSize ? prev : current
        }
       );
-      return {caption: photo.caption, fullImageUrl: fullImage.url, thumbnailUrl: thumbnail.url, assetType: photo.mediaAssetType ?? "image"}
+      return {caption: photo.caption, fullImageUrl: fullImage.url ?? "", thumbnailUrl: thumbnail.url ?? "gh ", assetType: photo.mediaAssetType ?? "image"}
     })
+    
     ctx.response.body = resp;
   });
-
+// Allow CORS from some URLS
+  app.use(
+    oakCors({
+      //TODO: Read this from ENV
+      origin:["http://localhost:1313","https://travel.igl-web.de", "https://traveldev.igl-web.de"],
+    }),
+  );
 app.use(router.allowedMethods());
 app.use(router.routes());
 
